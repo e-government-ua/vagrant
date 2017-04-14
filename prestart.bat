b6057c9e2b397a58a65096791df0e7ec3793e23b
Yurii Kolesnykov <yurikoles@gmail.com>
2017-04-14 20:18:20 +0300

    refactor windows script

--- prestart.bat
+++ prestart.bat
@@ -5,7 +5,4 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 # Install all dependencies using chocolatey
-choco install virtualbox -y
-choco install virtualbox.extensionpack -y
-choco install vagrant -y
-choco install git -y
+cinst virtualbox virtualbox.extensionpack vagrant git -y
